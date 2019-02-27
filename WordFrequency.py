e047300efae119e2364a8b0297cd89fa6298094c
WPrendota <wibson90@gmail.com>
2019-02-27 22:03:06 +0100

    Initial commit

---
+++ WordFrequency.py
@@ -0,0 +1,54 @@
+# Slow version:
+
+import re
+
+#-------------------------------------------------------------
+
+text = open(r"C:\Users\Witek\Desktop\pan-tadeusz.txt", encoding="utf8").read()
+words = text.replace("\n", " ")
+words = re.sub('[^A-Za-z0-9ąćęłńóśźż]+', ' ', words)
+words = words.split(' ')
+words.sort()
+
+#-------------------------------------------------------------
+
+wor = []
+
+def word_find(word, words):
+    count = 0
+
+    for x, x_word in enumerate(words):
+        if word == x_word:
+            count +=1
+
+        if x == words.__len__()-1:
+            return count
+
+    return False
+
+#-------------------------------------------------------------
+
+def word_frequency(words_len, count):
+    word_f = count/words_len
+
+    return word_f
+
+#-------------------------------------------------------------
+
+f_save = open("counted_words.txt","w")
+
+for x, word in enumerate(words):
+    count = word_find(word, words)
+    freq = word_frequency(words.__len__(), count)
+
+    if count > 1:
+        del words[x+1:x+count]
+
+    wor.append([[word], [count], [freq]])
+
+    f_save.write(word+" "+str(count)+" "+str(freq)+"\n")
+
+    count = 0
+    freq = 0
+
+print(wor)
